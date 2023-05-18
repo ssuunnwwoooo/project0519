487b7110f547a2bf9634ca6e5f5fb5c211c9fd9d
sunwoo <133613205+ssuunnwwoooo@users.noreply.github.com>
2023-05-18 18:08:21 +0900

    19일까지 제출하는 과제

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+    $('.mainSlide').slick({
+        // arrows: ture,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        pauseOnHover: false,
+    });
+})
